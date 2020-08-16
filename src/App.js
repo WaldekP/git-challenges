0458e58be7c010c34c866ddcbdb5d714b337edf3
Waldek <waldek.pieniak@gmail.com>
2020-08-16 16:59:03 +0200

    Amended message

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Git mistakes
         </a>
       </header>
     </div>
