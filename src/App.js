5f75a49b91697f324e17a5af314867c59a10bd99
Waldek <waldek.pieniak@gmail.com>
2020-08-16 17:17:05 +0200

    App file changed and test component added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TestComponent } from "./components/testComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +17,7 @@ function App() {
           Git mistakes
         </a>
       </header>
+        <TestComponent />
     </div>
   );
 }
